de26adb4af39cb750b7034582e7bdb0f5a5425f8
Gadpande, Niyati <Niyati.Gadpande@cdk.com>
2019-02-03 23:00:42 +0530

    Add build file

---
+++ build/precache-manifest.9c6dbf2d4d4f52bce0af06d1a11be226.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1374a103223144cf1de1",
+    "url": "/todo.io/static/js/runtime~main.1374a103.js"
+  },
+  {
+    "revision": "5b8ba02684ee172d0fa3",
+    "url": "/todo.io/static/js/main.5b8ba026.chunk.js"
+  },
+  {
+    "revision": "c690a25cc9780649cc84",
+    "url": "/todo.io/static/js/1.c690a25c.chunk.js"
+  },
+  {
+    "revision": "5b8ba02684ee172d0fa3",
+    "url": "/todo.io/static/css/main.c871163d.chunk.css"
+  },
+  {
+    "revision": "af1d13ef2d28ed4d3bb55ae78a6dd798",
+    "url": "/todo.io/index.html"
+  }
+];
