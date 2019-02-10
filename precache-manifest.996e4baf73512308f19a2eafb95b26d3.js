b1f2c6db8d10933605ecb2af766834d6c2fca378
Gadpande, Niyati <Niyati.Gadpande@cdk.com>
2019-02-10 23:16:49 +0530

    Updates

---
+++ precache-manifest.996e4baf73512308f19a2eafb95b26d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "20242ed7bd1b63e88a71",
+    "url": "/todo/static/js/main.20242ed7.chunk.js"
+  },
+  {
+    "revision": "c690a25cc9780649cc84",
+    "url": "/todo/static/js/1.c690a25c.chunk.js"
+  },
+  {
+    "revision": "20242ed7bd1b63e88a71",
+    "url": "/todo/static/css/main.6b2b741b.chunk.css"
+  },
+  {
+    "revision": "053ce1641c550025cb353e01bb3b75fa",
+    "url": "/todo/index.html"
+  }
+];
